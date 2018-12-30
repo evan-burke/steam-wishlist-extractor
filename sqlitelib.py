c87977b4c31b9d93e5ff8ac13108f1e4d9c428f4
Evan Burke <evan.burke+git@gmail.com>
2018-12-30 12:40:33 -0600

    add sqlite wrapper

---
+++ sqlitelib.py
@@ -0,0 +1,55 @@
+
+""" This is pretty basic but makes it a little eaiser to use sqlite3 for common tasks.
+
+    Example usage:
+    db = sqlitelib(dbfile)
+    result = db.execute("select * from mytable")
+    
+    or:
+    with sqlitelib(dbfile) as db:
+      result = db.execute("select * from  mytable")
+    
+    """
+
+import sqlite3
+
+class sqlitelib(object):
+    """ wrapper for sqlite3 providing some convenience functions.
+        Autocommit is on by default, and query results are lists of dicts. """
+    
+    def __init__(self, dbfile):
+        self.dbfile = dbfile
+        self.conn = self.connect()
+        
+    def __enter__(self):
+        return self #.__init__()
+        
+    def connect(self):
+        self.conn = sqlite3.connect(self.dbfile, isolation_level=None)
+        self.conn.row_factory = self.dict_factory
+        return self.conn
+    
+    def execute(self, sql, parameters=None):
+        if not parameters:
+            c = self.conn.execute(sql)
+        else:
+            c = self.conn.execute(sql, parameters)
+        data = c.fetchall()
+        return data
+    
+    def executemany(self, sql, parameters):
+        c = self.conn.executemany(sql, parameters)
+        data = c.fetchall()
+        return data
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+
+    def close(self):
+        self.conn.close()
+        
+    def dict_factory(self, cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
